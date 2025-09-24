affd2229b1b62f0284c0e7d0677feafc1e282f5a
m4nu-git <manudev912@gmail.com>
2025-09-24 11:57:02 +0530

    feat: Added member api to check subscription of a workspace

--- src/routes/v1/v1Router.js
+++ src/routes/v1/v1Router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import channelRouter from "./channel.js"
+import memberRouter from './members.js';
 import userRouter from "./users.js"
 import workspaceRouter from "./workspaces.js"
 
@@ -9,5 +10,6 @@ const router = express.Router();
 router.use('/users', userRouter);
 router.use('/workspaces', workspaceRouter);
 router.use('/channels', channelRouter);
+router.use('/members', memberRouter);
 
 export default router;
